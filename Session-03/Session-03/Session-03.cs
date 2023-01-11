2d46ebed469ef4c777816c0d946153519dfacb2b
Anestis Melidis <anestis.melidis97@gmail.com>
2023-01-11 13:54:19 +0200

    Add project files.

---
+++ Session-03/Session-03/Session-03.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, Epsilon-Net, i am making my first baby steps to be a Software Developer ! ");
+
+Console.ReadLine();
