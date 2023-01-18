3d2da8121b41b0cb49595726e6b1e65c19c83e9b
Anestis Melidis <anestis.melidis97@gmail.com>
2023-01-18 12:21:33 +0200

    ActionResponse

--- Session-07/Session-07/ActionResponse.cs
+++ Session-07/Session-07/ActionResponse.cs
@@ -5,31 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Session_07 {
-    public class ActionResponse {
 
-        // PROPERTIES
-        public Guid RequestID { get; set; }
+    public class ActionResponse : ActionEntity {
+
         public Guid ResponseID { get; set; }
+        public string Output { get; set; }
 
-        public string? Output { get; set; }
-
-        // CTOR
         public ActionResponse() {
-        }
-        public ActionResponse(Guid requestID) {
-
-        }
-        public ActionResponse(Guid requestID ,Guid responseID) {
-
-        }
-        public ActionResponse(Guid requestID, Guid responseID,string? output) {
-
-        }
-
-        //methods
-        public virtual void CheckAndAct() {
-
-         // return null;
+            ResponseID = Guid.NewGuid();
         }
     }
+
 }
