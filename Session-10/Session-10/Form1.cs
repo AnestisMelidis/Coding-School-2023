f177b37e7a9cac171ded5d76c113666e7f8e48d6
Anestis Melidis <anestis.melidis97@gmail.com>
2023-01-20 13:51:42 +0200

    Buttons

--- Session-10/Session-10/Form1.cs
+++ Session-10/Session-10/Form1.cs
@@ -3,5 +3,13 @@ namespace Session_10 {
         public Form1() {
             InitializeComponent();
         }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e) {
+
+        }
     }
 }
