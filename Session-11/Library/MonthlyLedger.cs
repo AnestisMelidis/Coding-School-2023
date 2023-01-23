b8ef80590d7ad30c39727a2766519b9c4d56cf22
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-01-23 16:33:06 +0200

    monthly ledger

---
+++ Session-11/Library/MonthlyLedger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class MonthlyLedger
+    {
+        //Properties
+        public DateTime Year { get; set; }
+        public DateTime Month { get; set; }
+        public double Income { get; set; }
+        public double Expenses { get; set; }
+        public double Total { get; set; }
+
+        //Constructors
+        public MonthlyLedger() 
+        {
+
+        }
+
+        //Methods
+        public void GetIncome(double income, DateTime date)
+        {
+
+        }
+    }
+}
