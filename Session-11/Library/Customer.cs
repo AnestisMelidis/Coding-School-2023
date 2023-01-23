304c657ff4650aff66b60213baed5999f011aac9
Anna Ilia <annailia9802@gmail.com>
2023-01-23 16:19:15 +0200

    Create Customer.cs

---
+++ Session-11/Library/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Customer
+{
+	public Customer()
+	{
+		public Guid ID { get; set; }
+		public int Code { get; set; }
+		public string Description { get; set; }
+	}
+}
