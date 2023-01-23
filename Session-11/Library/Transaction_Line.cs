3c5afe054a55867832d4f535ad216e6d567206d2
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-23 17:03:11 +0200

    Classes Transanction- Line

---
+++ Session-11/Library/Transaction_Line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Transaction
+    {
+        public Guid ID { get; set; }
+        public Guid ProductID { get; set; }
+        public Datetime Date { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+        public double TotalPrice { get; set; }
+        public Transaction{
+            }
+        public void DiscountCheck(){}
+}
