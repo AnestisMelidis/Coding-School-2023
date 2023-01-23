69ce611d5e0f3cab7cdedb3a4e83ce42766cadf4
Anestis Melidis <anestis.melidis97@gmail.com>
2023-01-23 16:30:06 +0200

    Push

--- Session-09/Session-09/Form1.Designer.cs
+++ Session-09/Session-09/Form1.Designer.cs
@@ -247,6 +247,7 @@
             this.Controls.Add(this.bntOne);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
