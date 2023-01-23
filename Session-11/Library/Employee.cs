a845e0910124ac57da718b7c7e7bb2fa9a21ca8a
Anna Ilia <annailia9802@gmail.com>
2023-01-23 16:38:35 +0200

    Create Employee.cs

---
+++ Session-11/Library/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Session_11{
+
+    public class Employee
+    {
+       
+       public Guid ID { get; set; }
+       public string Name { get; set; }
+       public string Surname { get; set; }
+       public enum EmployeeType { get; set; }
+       public double SallaryPerMonth { get; set; }
+    }
+
+    /* Each shop should have: 1 Manager, 1-2 Cashiers, 1-2 Baristas and 1-3 Waiters */
+
+    public enum EmployeeType    
+    {
+        Manager,
+        Cashier,
+        Barista,
+        Waiter
+    }
+
+ }
+
