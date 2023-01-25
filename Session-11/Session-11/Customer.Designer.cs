597b579c4e7c5c0253fc3fa2b57bea6df7cdabbb
Γεώργιος Κοσμαράς <giorgio2025@gmail.com>
2023-01-25 21:16:43 +0200

    customer form complete

---
+++ Session-11/Session-11/Customer.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Session_11
+{
+    partial class Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbMenu = new System.Windows.Forms.ComboBox();
+            this.chkFood = new System.Windows.Forms.CheckBox();
+            this.chkBeverages = new System.Windows.Forms.CheckBox();
+            this.chkCoffee = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cmbMenu
+            // 
+            this.cmbMenu.FormattingEnabled = true;
+            this.cmbMenu.Location = new System.Drawing.Point(101, 62);
+            this.cmbMenu.Name = "cmbMenu";
+            this.cmbMenu.Size = new System.Drawing.Size(83, 23);
+            this.cmbMenu.TabIndex = 34;
+            // 
+            // chkFood
+            // 
+            this.chkFood.AutoSize = true;
+            this.chkFood.Location = new System.Drawing.Point(210, 29);
+            this.chkFood.Name = "chkFood";
+            this.chkFood.Size = new System.Drawing.Size(53, 19);
+            this.chkFood.TabIndex = 33;
+            this.chkFood.Text = "Food";
+            this.chkFood.UseVisualStyleBackColor = true;
+            this.chkFood.Click += new System.EventHandler(this.chkFood_Checked);
+            // 
+            // chkBeverages
+            // 
+            this.chkBeverages.AutoSize = true;
+            this.chkBeverages.Location = new System.Drawing.Point(121, 29);
+            this.chkBeverages.Name = "chkBeverages";
+            this.chkBeverages.Size = new System.Drawing.Size(79, 19);
+            this.chkBeverages.TabIndex = 32;
+            this.chkBeverages.Text = "Beverages";
+            this.chkBeverages.UseVisualStyleBackColor = true;
+            this.chkBeverages.Click += new System.EventHandler(this.chkBevarages_Checked);
+            // 
+            // chkCoffee
+            // 
+            this.chkCoffee.AutoSize = true;
+            this.chkCoffee.Location = new System.Drawing.Point(32, 29);
+            this.chkCoffee.Name = "chkCoffee";
+            this.chkCoffee.Size = new System.Drawing.Size(61, 19);
+            this.chkCoffee.TabIndex = 31;
+            this.chkCoffee.Text = "Coffee";
+            this.chkCoffee.UseVisualStyleBackColor = true;
+            this.chkCoffee.Click += new System.EventHandler(this.chkCofee_Checked);
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbMenu);
+            this.Controls.Add(this.chkFood);
+            this.Controls.Add(this.chkBeverages);
+            this.Controls.Add(this.chkCoffee);
+            this.Name = "Customer";
+            this.Text = "Customer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox cmbMenu;
+        private CheckBox chkFood;
+        private CheckBox chkBeverages;
+        private CheckBox chkCoffee;
+    }
+}
---
+++ Session-11/Session-11/Customer.cs
@@ -0,0 +1,79 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Library.Product;
+
+namespace Session_11
+{
+    public partial class Customer : Form
+    {
+        public CoffeeShopData Data { get; set; }
+        private CoffeeShopData _CoffeeShopData = new CoffeeShopData();
+        public Customer(CoffeeShopData test)
+        {
+            InitializeComponent();
+            _CoffeeShopData = test;
+        }
+
+        private void chkCofee_Checked(object sender, EventArgs e)
+        {
+            
+            if (chkCoffee.Checked == true)
+            {
+                foreach (var i in _CoffeeShopData.products)
+                {
+                    if (i.TypeOfProduct == ProductType.Coffee)
+                    {
+                        cmbMenu.Items.Add(i.Description);
+                    }
+                }
+            }
+            if (chkCoffee.Checked == false)
+            {
+                cmbMenu.Items.Clear();
+            }
+        }
+        private void chkBevarages_Checked(object sender, EventArgs e)
+        {
+            if (chkBeverages.Checked == true)
+            {
+                foreach (var i in _CoffeeShopData.products)
+                {
+                    if (i.TypeOfProduct == ProductType.Beverages)
+                    {
+                        cmbMenu.Items.Add(i.Description);
+                    }
+                }
+            }
+            if (chkBeverages.Checked == false)
+            {
+                cmbMenu.Items.Clear();
+            }
+        }
+        private void chkFood_Checked(object sender, EventArgs e)
+        {
+            if (chkFood.Checked == true)
+            {
+                foreach (var i in _CoffeeShopData.products)
+                {
+                    if (i.TypeOfProduct == ProductType.Food)
+                    {
+                        cmbMenu.Items.Add(i.Description);
+                    }
+                }
+            }
+            if (chkFood.Checked == false)
+            {
+                cmbMenu.Items.Clear();
+            }
+        }
+
+    }
+}
