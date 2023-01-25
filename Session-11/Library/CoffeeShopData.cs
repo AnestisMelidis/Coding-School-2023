658faebeeff1b5e84e76f85fc24e93a22b90a6c7
Katerina <Kater@LAPTOP-735L895G>
2023-01-25 19:08:07 +0200

    kate changes

--- Session-11/Library/CoffeeShopData.cs
+++ Session-11/Library/CoffeeShopData.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Library {
-    public class CoffeeShopData {
+namespace Library
+{
+    public class CoffeeShopData
+    {
+
+        // properties
+        public List<Product> Products { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+
+        public List<TransactionLine> TransactionLines { get; set; }
+
+
+
+
+        // constructors
+        public CoffeeShopData()
+        {
+            Products = new List<Product>();
+            Employees = new List<Employee>();
+            TransactionLines = new List<TransactionLine>();
+
+
+
+        }
+
 
     }
 }
+
